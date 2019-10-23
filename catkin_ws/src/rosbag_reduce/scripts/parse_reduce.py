455d1220737fbe36aa94b7fe000cf9ea40c92879
haydenm2 <haydenmmorgan@gmail.com>
2019-10-23 07:30:57 -0600

    functional rosbag reduction through python parsing

--- catkin_ws/src/rosbag_reduce/scripts/parse_reduce.py
+++ catkin_ws/src/rosbag_reduce/scripts/parse_reduce.py
@@ -1,52 +1,57 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import sys
 import rosbag
-from std_msgs.msg import Int32, String
+import cv2
 from sensor_msgs.msg import Image
-from rosgraph_msgs.msg import Clock
 from cv_bridge import CvBridge, CvBridgeError
 
+# initialization variables
+init = True
+
+# import/export parameters/objects
 in_bag_name = '/home/haydenm2/bagfiles/data_054_2019-08-20T08_57_14.bag'
 out_bag_name = '/home/haydenm2/bagfiles/reduced.bag'
-
 img_topic = '/pylon_camera_node/image_raw'
-
+resize_scale = 0.01
 in_bag = rosbag.Bag(in_bag_name)
 out_bag = rosbag.Bag(out_bag_name, 'w')
 
+# progress readout parameters
 t_begin = in_bag.get_start_time()
 t_end = in_bag.get_end_time()
 t_total = t_end - t_begin
 
+# CvBridge init
+bridge = CvBridge()
+
+# iterate through incoming bag
 for topic, msg, t in in_bag.read_messages(topics=[img_topic]):  
   I = Image()
   I = msg
+  enc = I.encoding
 
-  out_bag.write(img_topic, I, t=t)
-    
-  # print("MSG: ", msg)
-  # print("TOPIC: ", topic)
-  # print("T: ", t)
+  try:
+    cv_image = bridge.imgmsg_to_cv2(I, enc)
+  except CvBridgeError as e:
+    print(e)
+
+  if(init):
+    img_h_reduced = int(cv_image.shape[0] * resize_scale)
+    img_w_reduced = int(cv_image.shape[1] * resize_scale)
+    reduced_dim = (img_w_reduced, img_h_reduced)
+
+  cv_image_reduced = cv2.resize(cv_image, reduced_dim, interpolation = cv2.INTER_AREA)
+
+  I_new = bridge.cv2_to_imgmsg(cv_image_reduced)
+  out_bag.write(img_topic, I_new, t=t)
   
-  print(f"Percent Complete: {(((t.to_sec()-t_begin)/t_total)*100):.2f}%\t\t", end='\r')
+  print "\r \033[1;31m Percent Complete: \033[0;0m %f %%" % (((t.to_sec()-t_begin)/t_total)*100),
   sys.stdout.flush()
   pass
 
-print("Rosbag Resolution Reduction Complete!")
-print("Output bag: ", out_bag_name)
+print("\n \033[1;32m Rosbag Resolution Reduction Complete! \033[0;0m")
+print("Output bag: %s \n" % out_bag_name)
 
 out_bag.close()
 in_bag.close()
-
-# try:
-#     c = Clock()
-#     c.data = 
-
-
-#     I = Image()
-#     I.data = 
-
-#     out_bag.write('chatter', s)
-# finally:
-#     bag.close()
